4eca126ce7311a5f85b462db47b29f7733bdb9f3
stxss <93729036+stxss@users.noreply.github.com>
2023-03-09 21:41:08 +0000

    Create the chess board, with indexes and letters to identify them, as well as apply a sketch idea of the printing of the scoreboard

---
+++ lib/display.rb
@@ -0,0 +1,62 @@
+require_relative "./text_styles"
+
+class Display
+    using TextStyles
+
+    attr_accessor :board, :moves
+
+  def initialize(board)
+    @board = board
+    @moves = []
+    add_move
+    show
+  end
+
+  def show
+    if RUBY_PLATFORM =~ /win32/ || RUBY_PLATFORM =~ /mingw/
+      system("cls")
+    else
+      system("clear")
+    end
+
+    puts <<~HEREDOC
+
+                    player1 - score
+
+    #{display}
+
+                    player2 - score
+
+    HEREDOC
+  end
+
+  def display
+    output = ""
+    @board.each_with_index do |i, row|
+        output += " " * 9 + "#{((row - 8).abs).to_s.bold}" + " "
+        i.each_with_index do |j, column|
+            if (row + column).odd?
+                output += j.bg_color(:red)
+            else
+                output += j.bg_color(:pink)
+            end
+        end
+        # system("tput cup 3 40")
+        # puts "1. e4 e2 "
+        # system("tput cup 4 40")
+        # puts "2. e3 e1"
+        # system("tput cup 5 40")
+        # puts "3. d6 h5"
+        # system("tput cup 0 0")
+        output += "\n"
+    end
+    output += "            a  b  c  d  e  f  g  h".bold
+    output
+  end
+
+  def add_move
+    output = ""
+
+    @moves << output
+  end
+end
