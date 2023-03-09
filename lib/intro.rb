2ce89bb7c6f5f638e740d4b64486ae6b46cd6297
stxss <93729036+stxss@users.noreply.github.com>
2023-03-09 19:32:44 +0000

    Create an introduction for the game

---
+++ lib/intro.rb
@@ -0,0 +1,26 @@
+require_relative "./text_styles"
+
+class Intro
+    using TextStyles
+
+    def initialize
+        puts <<~HEREDOC
+        #{"Welcome to Chess!".bold.italic.underlined}
+
+        Chess is a board game between two players. It is played on a chessboard with 64 squares arranged in an eight-by-eight grid.
+
+        At the start, each player controls sixteen pieces:
+
+            - One king
+            - One queen
+            - Two rooks
+            - Two bishops
+            - Two knights
+            - Eight pawns
+
+        The player controlling the #{"white".bold.fg_color(:white)} pieces moves first, followed by the player controlling the #{"black".bold.fg_color(:black)} pieces.
+
+        The object of the game is to #{"checkmate".bold.italic} the opponent's king, whereby the king is under immediate attack (in "check") and there is no way for it to escape. There are also several ways a game can end in a draw.
+      HEREDOC
+    end
+end
