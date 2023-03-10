ebcca305717bb54f9ab3b0752478213fb4b4ef64
stxss <93729036+stxss@users.noreply.github.com>
2023-03-10 21:36:05 +0000

    Create the loop that allows for the constant checking for user input

--- lib/main.rb
+++ lib/main.rb
@@ -2,12 +2,17 @@ require_relative("text_styles")
 require_relative("display")
 require_relative("board")
 require_relative("game")
+require_relative("cursor")
 require_relative("intro")
 require_relative("player")
 require_relative("prompts")
 
 board = Board.new
-p grid = board.grid
+grid = board.grid
 display = Display.new(grid)
 board.populate
-display.show
+loop do
+  display.show
+  display.cursor.ask_input
+end
+
