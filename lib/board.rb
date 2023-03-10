cb2a52112e19699a01074f884d1d89a2685c9710
stxss <93729036+stxss@users.noreply.github.com>
2023-03-10 14:58:45 +0000

    Add method to populate the board with the pieces

--- lib/board.rb
+++ lib/board.rb
@@ -11,6 +11,28 @@ class Board
   attr_accessor :grid
 
   def initialize
-    @grid = Array.new(8, Array.new(8, "   "))
+    @grid = Array.new(8) { Array.new(8, "   ") }
+  end
+
+  def populate
+    pieces_row(0, :white)
+    pawn_row(1, :white)
+    pawn_row(6, :black)
+    pieces_row(7, :black)
+  end
+
+  def pawn_row(row, color)
+    @grid[row].map! { Pawn.new(color).symbol }
+  end
+
+  def pieces_row(row, color)
+    @grid[row][0] = Rook.new(color).symbol
+    @grid[row][1] = Bishop.new(color).symbol
+    @grid[row][2] = Knight.new(color).symbol
+    @grid[row][3] = Queen.new(color).symbol
+    @grid[row][4] = King.new(color).symbol
+    @grid[row][5] = Bishop.new(color).symbol
+    @grid[row][6] = Knight.new(color).symbol
+    @grid[row][7] = Rook.new(color).symbol
   end
 end
