bbd9e3ce90f08c7453d2fb6c41d2e3ef413adf91
stxss <93729036+stxss@users.noreply.github.com>
2023-03-12 17:54:21 +0000

    Add color identification to the Piece class, as well as freezing the pieces array

--- lib/pieces/piece.rb
+++ lib/pieces/piece.rb
@@ -7,15 +7,16 @@ PIECES = {
   queen:  " \u{265B} ",
   king:   " \u{265A} ",
   pawn:   " \u{265F} "
-}
+}.freeze
 
 class Piece
   using TextStyles
 
-  attr_accessor :valid_moves, :symbol
+  attr_accessor :valid_moves, :symbol, :color, :piece
 
   def initialize(piece, color)
     @valid_moves = []
+    @color = color
     @piece = PIECES[piece]
     @symbol = paint(color)
   end
