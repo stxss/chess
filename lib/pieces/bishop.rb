def6f5f65481afa859b111225df2524b24f9fbfa
stxss <93729036+stxss@users.noreply.github.com>
2023-03-14 18:28:34 +0000

    Add basic movement directions for Bishop

--- lib/pieces/bishop.rb
+++ lib/pieces/bishop.rb
@@ -1,5 +1,14 @@
 require_relative("./../movement/directions")
 
 class Bishop
-    
+  include Directions
+
+  def movement(board, start_position, piece)
+    @board = board
+    @start_position = start_position
+    @piece = piece.piece
+    @color = piece.color
+    directions = [[-1, -1], [-1, 1], [1, -1], [1, 1]]
+    find_moves(:bishop, directions)
+  end
 end
