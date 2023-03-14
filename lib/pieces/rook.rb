7297d75d9f2934be77a12ea567620fcf432e0caf
stxss <93729036+stxss@users.noreply.github.com>
2023-03-14 18:30:14 +0000

    Add basic movement directions for Rook

--- lib/pieces/rook.rb
+++ lib/pieces/rook.rb
@@ -1,14 +1,15 @@
 require_relative("./../movement/directions")
+require_relative("./../pieces/empty_square")
 
 class Rook
-  include Directions::Vertical
-  include Directions::Horizontal
+  include Directions
 
   def movement(board, start_position, piece)
     @board = board
     @start_position = start_position
-    @piece = piece
-
-    vertical + horizontal
+    @piece = piece.piece
+    @color = piece.color
+    directions = [[-1, 0], [1, 0], [0, -1], [0, 1]] # up, down, left, right
+    find_moves(:rook, directions)
   end
 end
