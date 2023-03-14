0ffbe455c46fb605dac78d7bdde0c5824b2010bb
stxss <93729036+stxss@users.noreply.github.com>
2023-03-14 18:23:54 +0000

    Add #find_moves

--- lib/movement/directions.rb
+++ lib/movement/directions.rb
@@ -1,36 +1,33 @@
 module Directions
-  module Vertical
-    def vertical
-      up + down
-    end
+  def find_moves(piece, directions)
+    row = @start_position[0]
+    col = @start_position[1]
+    enemies = []
+    empties = []
 
-    def up
-      moves = []
+    directions.each do |direction|
+      next_row = row + direction[0]
+      next_col = col + direction[1]
 
-      @board.grid.select.with_index do |row, i|
-        row.select.with_index do |col, j|
-          moves << "self" if @start_position == [i, j]
-          if j == @start_position[1] && i < @start_position[0]
-            moves << col.piece
-          end
+      while next_row.between?(0, 7) && next_col.between?(0, 7)
+        if ally?(@color, [next_row, next_col])
+          break
+        elsif enemy?(@color, [next_row, next_col])
+          enemies << [next_row, next_col]
+          break
+        elsif empty?([next_row, next_col])
+          empties << [next_row, next_col]
         end
-      end
-      moves
-    end
 
-    def down
-      moves = []
-
-      @board.grid.select.with_index do |row, i|
-        row.select.with_index do |col, j|
-          if j == @start_position[1] && i > @start_position[0]
-
-            moves << col.piece
-          end
+        case piece
+        when :pawn, :knight, :king
+          break
         end
+        next_row += direction[0]
+        next_col += direction[1]
       end
-      moves
     end
+    empties + enemies
   end
 
   module Horizontal
