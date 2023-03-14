189b83ec71ad60d7a3c54ea4b787833b675276bf
stxss <93729036+stxss@users.noreply.github.com>
2023-03-14 18:29:25 +0000

    Add basic movement directions for Knight

--- lib/pieces/knight.rb
+++ lib/pieces/knight.rb
@@ -1,4 +1,14 @@
 require_relative("./../movement/directions")
 
 class Knight
+  include Directions
+
+  def movement(board, start_position, piece)
+    @board = board
+    @start_position = start_position
+    @piece = piece.piece
+    @color = piece.color
+    directions = [[-2, -1], [-2, 1], [-1, -2], [-1, 2], [1, -2], [1, 2], [2, -1], [2, 1]] # up, down, left, right
+    find_moves(:knight, directions)
+  end
 end
