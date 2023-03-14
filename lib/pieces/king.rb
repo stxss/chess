72f208642f695075c2f6238256edab36bdb812b6
stxss <93729036+stxss@users.noreply.github.com>
2023-03-14 18:29:07 +0000

    Add basic movement directions for King

--- lib/pieces/king.rb
+++ lib/pieces/king.rb
@@ -1,5 +1,16 @@
 require_relative("./../movement/directions")
 
 class King
+  include Directions
 
+  def movement(board, start_position, piece)
+    @board = board
+    @start_position = start_position
+    @piece = piece.piece
+    @color = piece.color
+    directions = [[-1, -1], [-1, 0], [-1, 1],
+      [0, -1], [0, 1],
+      [1, -1], [1, 0], [1, 1]] # up, down, left, right
+    find_moves(:king, directions)
+  end
 end
