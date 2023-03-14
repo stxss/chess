072029fb54e930c6d8eaef22cf8861c3539c4ffb
stxss <93729036+stxss@users.noreply.github.com>
2023-03-14 18:29:40 +0000

    Add basic movement directions for Pawn

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -1,5 +1,14 @@
 require_relative("./../movement/directions")
 
 class Pawn
+  include Directions
 
+  def movement(board, start_position, piece)
+    @board = board
+    @start_position = start_position
+    @piece = piece.piece
+    @color = piece.color
+    directions = [[-1, 0], [-2, 0]] # up, down, left, right
+    find_moves(:pawn, directions)
+  end
 end
