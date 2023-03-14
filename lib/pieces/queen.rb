af70433fdade4a39149be6406382aae1fbb80629
stxss <93729036+stxss@users.noreply.github.com>
2023-03-14 18:29:57 +0000

    Add basic movement directions for Queen

--- lib/pieces/queen.rb
+++ lib/pieces/queen.rb
@@ -1,7 +1,14 @@
 require_relative("./../movement/directions")
 
 class Queen
-    include Directions
+  include Directions
 
-
+  def movement(board, start_position, piece)
+    @board = board
+    @start_position = start_position
+    @piece = piece.piece
+    @color = piece.color
+    directions = [[-1, 0], [1, 0], [0, -1], [0, 1], [-1, -1], [-1, 1], [1, -1], [1, 1]] # up, down, left, right, up_left, up_right, down_left, down_right
+    find_moves(:queen, directions)
+  end
 end
