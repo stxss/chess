18942ac50ccfdeff1f81628fb51936f23ca70cc4
stxss <93729036+stxss@users.noreply.github.com>
2023-03-15 14:10:13 +0000

    Add some pawn movement
    Add the checks for the pawns normal movement, according to color, as
    they can only move 'ahead' and never 'back', as well as add a special
    check to see if they have moved at least once, as in their first play,
    they can jump 2 squares. Also, Pawns can never take ahead, only
    diagonally, so there's a check for those enemies too.

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -8,12 +8,44 @@ class Pawn
     @start_position = start_position
     @piece = piece.piece
     @color = piece.color
+
     case @color
     when :white
-      directions = [[-1, 0], [-2, 0]] # up, down, left, right
+      directions = moved_once?(@color) ? [[-1, 0]] : [[-1, 0], [-2, 0]]
+      enemy_directions = [[-1, -1], [-1, 1]]
     when :black
-      directions = [[1, 0], [2, 0]]
+      directions = moved_once?(@color) ? [[1, 0]] : [[1, 0], [2, 0]]
+      enemy_directions = [[1, -1], [1, 1]]
     end
-    find_moves(:pawn, directions)
+    find_moves(:pawn, directions) + pawn_enemies(@color, enemy_directions)
+  end
+
+  private
+
+  def moved_once?(color)
+    # starting pawn rows, column is not relevant for this check
+    w_pawn_start = 6
+    b_pawn_start = 1
+
+    case color
+    when :white
+      @start_position[0] != w_pawn_start
+    when :black
+      @start_position[0] != b_pawn_start
+    end
+  end
+
+  def pawn_enemies(color, directions)
+    row = @start_position[0]
+    col = @start_position[1]
+    enemies = []
+
+    directions.each do |direction|
+      next_row = row + direction[0]
+      next_col = col + direction[1]
+
+      enemies << [next_row, next_col] if enemy?(color, [next_row, next_col])
+    end
+    enemies
   end
 end
