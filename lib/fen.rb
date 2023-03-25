8e71bef0349695ea867a1250574803c9cdce3c43
stxss <93729036+stxss@users.noreply.github.com>
2023-03-25 16:50:41 +0000

    Add #piece_placement to FEN serializer

--- lib/fen.rb
+++ lib/fen.rb
@@ -1,13 +1,20 @@
 module Fen
   def to_fen
+    piece_placement + active_color + castling_rights + en_passant_targets + half_moves + full_moves
+  end
+
+  def piece_placement
     result = ""
     @board.grid.each_with_index do |i, row_index|
-        empty_count = 0
+      empty_count = 0
       i.each_with_index do |piece, column_index|
         if piece.painted == "   "
           empty_count += 1
           next
         end
+
+        result << empty_count.to_s unless empty_count.zero?
+        empty_count = 0
         result << piece.fen_notation.to_s
       end
       result << empty_count.to_s unless empty_count.zero?
